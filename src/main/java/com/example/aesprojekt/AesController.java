55c7689aff5dd5354dce0279f7e541fb0286538e
Erëza Ismajli <94221195+erezaismajlii@users.noreply.github.com>
2023-01-06 19:34:19 +0100

    Implementimi i AES Controller

--- src/main/java/com/example/aesprojekt/AesController.java
+++ src/main/java/com/example/aesprojekt/AesController.java
@@ -1,9 +1,45 @@
 package com.example.aesprojekt;
 
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 public class AesController {
 
+    Encryptor encryptor = new Encryptor();
 
+    @FXML
+    private TextField keyTextField;
+
+    @FXML
+    private TextField inputTextField;
+
+    @FXML
+    private TextField outputTextField;
+
+    @FXML
+    void decryptButton(ActionEvent event) throws NoSuchPaddingException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+        // Could be 128, 192, or 256 bits.
+        byte[] key = encryptor.stringToByteArray(keyTextField.getText());
+        String input = inputTextField.getText();
+        String encryptedString = encryptor.decrypt(input,key);
+        outputTextField.setText(encryptedString);
     }
 
+
+    @FXML
+    void encryptButton(ActionEvent event) throws NoSuchPaddingException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+        //Could be 128, 192, or 256 bits.
+        byte[] key = encryptor.stringToByteArray(keyTextField.getText());
+        String input = inputTextField.getText();
+
+        String encryptedString = encryptor.encrypt(input,key);
+        outputTextField.setText(encryptedString);
+    }
+}
