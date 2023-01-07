3eed27403bfc7def8a33301213d3f8059cf87d81
Blerina <blerina.beka@student.uni-pr.edu>
2023-01-07 14:25:37 +0100

    Permisimi i nje fotoje

--- src/main/java/fiek/siguriainternet/aesprojekt/AesApplication.java
+++ src/main/java/fiek/siguriainternet/aesprojekt/AesApplication.java
@@ -2,15 +2,22 @@ package fiek.siguriainternet.aesprojekt;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class AesApplication extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(AesApplication.class.getResource("aes-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+
+        InputStream stream = new FileInputStream("src/main/resources/figurat/aes.jpg");
+        Image image = new Image(stream);
+        stage.getIcons().add(image);
         stage.setTitle("AES enkriptim");
         stage.setScene(scene);
         stage.show();
